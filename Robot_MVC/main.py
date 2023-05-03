38a0755ca8142933b3d4f472b9279e6bdc7e41b8
David Freddy <111796950+frd2377@users.noreply.github.com>
2023-05-03 09:28:28 -0500

    Update main.py

--- Robot_MVC/main.py
+++ Robot_MVC/main.py
@@ -3,7 +3,7 @@ from model import Robot
 from controller import Robot_Controller
 from view import Robot_View
 
-robot = Robot([0,0,0],[0,0,0])
+robot = Robot()
 view = Robot_View()
 controller = Robot_Controller(robot,view)
 view.controller = controller
